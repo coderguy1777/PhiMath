e315f25162d5ec9846c5a4758c498211eff954a8
coderguy1777 <hill.jordan@charterschool.org>
2019-09-11 15:47:02 -0400

    work on the vec3 class more, added unit vec func.

--- phimath/vectors/vector3.py
+++ phimath/vectors/vector3.py
@@ -1,11 +1,15 @@
 from phimath.vectors.point3d import Point3D
+import math
+
 class Vec3:
     def __init__(self, vec=Point3D):
         self.vec = vec
-        # simple "overloads" for easy code
         self.mult_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() * vec_y.get_x() + vec_x.get_y() * vec_y.get_y() + vec_x.get_z() * vec_y.get_z()
+
         self.add_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() + vec_y.get_x() + vec_x.get_y() + vec_y.get_y() + vec_x.get_z() + vec_y.get_z()
+        
         self.div_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() / vec_y.get_x() + vec_x.get_y() / vec_y.get_y() + vec_x.get_z() / vec_y.get_z()
+
         self.sub_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() + vec_y.get_x() + vec_x.get_y() + vec_y.get_y() + vec_x.get_z() + vec_y.get_z()
 
     def sub_vec3d(self, vecb=Point3D):
@@ -29,3 +33,12 @@ class Vec3:
         self.vec.change_y(self.vec.get_y() * scalar)
         self.vec.change_z(self.vec.get_z() * scalar)
         return self.vec.get_group()
+
+    def vec_mag(self):
+        return math.sqrt(pow(self.vec.get_x(), 2) + pow(self.vec.get_y(), 2) + pow(self.vec.get_z(), 2))
+    
+    def vec_to_unit_vec(self, mag):
+        return Vec3(self.vec.get_x()/mag, self.vec.get_y()/mag, self.vec.get_z()/mag)
+
+    def unit_vec_mag(self, mag):
+        return math.sqrt(pow(self.vec.get_x()/mag, 2) + pow(self.vec.get_y()/mag, 2) + pow(self.vec.get_z()/mag, 2))
