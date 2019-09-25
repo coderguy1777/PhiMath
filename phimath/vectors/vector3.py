00c23a0f40afa45562bc4bd1334f68681fc0b4f1
coderguy1777 <hill.jordan@charterschool.org>
2019-09-25 19:59:33 -0400

    more vec3 stuff

--- phimath/vectors/vector3.py
+++ phimath/vectors/vector3.py
@@ -5,11 +5,8 @@ class Vec3:
     def __init__(self, vec=Point3D):
         self.vec = vec
         self.mult_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() * vec_y.get_x() + vec_x.get_y() * vec_y.get_y() + vec_x.get_z() * vec_y.get_z()
-
         self.add_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() + vec_y.get_x() + vec_x.get_y() + vec_y.get_y() + vec_x.get_z() + vec_y.get_z()
-        
         self.div_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() / vec_y.get_x() + vec_x.get_y() / vec_y.get_y() + vec_x.get_z() / vec_y.get_z()
-
         self.sub_vec = lambda vec_x=Point3D, vec_y=Point3D : vec_x.get_x() + vec_y.get_x() + vec_x.get_y() + vec_y.get_y() + vec_x.get_z() + vec_y.get_z()
 
     def sub_vec3d(self, vecb=Point3D):
