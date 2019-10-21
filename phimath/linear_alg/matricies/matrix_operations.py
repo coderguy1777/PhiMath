997cc0ddacd29bd8b2848136b089a177e6b3458b
coderguy1777 <hill.jordan@charterschool.org>
2019-10-21 12:05:39 -0400

    matrix operations started

--- phimath/linear_alg/matricies/matrix_operations.py
+++ phimath/linear_alg/matricies/matrix_operations.py
@@ -1,1 +1,12 @@
-import numpy
+from phimath.linear_alg.matricies import matrix 
+
+class MatrixOperations:
+    def __init__(self):
+        self.matrix_a = 0
+        self.matrix_b = 0
+
+    def set_mat_a(self, mat_a=matrix):
+        self.matrix_a = mat_a
+    
+    def set_mat_b(self, mat_b=matrix):
+        self.matrix_b = mat_b
